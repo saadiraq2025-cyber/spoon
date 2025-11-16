c678f9db1fba2fb2c26c657e63dca561500453a1
saadiraq2025-cyber <saadiraq2025@gmail.com>
2025-11-16 23:34:10 +0300

    Add Firebase configuration and initialization script

---
+++ firebase.js
@@ -0,0 +1,16 @@
+<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-database-compat.js"></script>
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyA_3TFx5dUR3JbcXj5jFIZ_mpjWeco7FVo",
+  authDomain: "tktkbaghdad.firebaseapp.com",
+  databaseURL: "https://tktkbaghdad-default-rtdb.firebaseio.com",
+  projectId: "tktkbaghdad",
+  storageBucket: "tktkbaghdad.firebasestorage.app",
+  messagingSenderId: "939931176033",
+  appId: "1:939931176033:web:1d44fa5fd01ee75b326e20"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+</script>
